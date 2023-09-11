0cb6a9f955cc81ddd964046cb6e8ef7985b55a5c
qaa0708 <qaa0708@users.noreply.github.comemail.com>
2023-09-11 16:01:12 +0900

    2주차 수업 끝

---
+++ src/w2/sketches/sketch2/script/alertPromptConfirm.js
@@ -0,0 +1,3 @@
+alert('나의 홈페이지에 온 걸 환영한다.');
+let YourName = prompt('네놈의 이름이 무엇이냐.', '홍길동');
+confirm('네놈이 이름이' + YourName + '라는 것이냐?');
---
+++ src/w2/sketches/sketch5/script/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(500, 500);
+}
+// 한 번만 실행
+function draw() {
+  background(255);
+  circle(mouseX, mouseY, 100);
+}
+// 계속 실행
